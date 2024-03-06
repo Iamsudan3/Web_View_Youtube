d21840c4b0a9d3ee046d9840f7e11b1c4c360e06
Iamsudan <khanalsudan89@gmail.com>
2024-03-06 21:21:07 +0545

    Somes changes

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -3,7 +3,6 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:webapp/screens/Home.dart';
 
-
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -12,32 +11,24 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
-
-@override
-void initState() {
-
-super.initState();
-  Timer(const Duration(seconds: 3), () {
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
-
-   });
-  
-  
-}
-
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
       body: Center(
-        
-        child:Image.asset(
-          "assets/youtube.png",
-          scale:5 ,
-          )
-          
-      ),
+          child: Image.asset(
+        "assets/youtube.png",
+        scale: 5,
+      )),
     );
   }
 }
